b3957a7e1d048dacc3caf809aeefe6efc1399a13
MasamichiMiyagi <114850152+MasamichiMiyagi@users.noreply.github.com>
2022-10-11 10:16:35 +0000

    [2022/10/11/19:16]bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,14 @@ class BooksController < ApplicationController
   end
 
   def index
+    @books = Book.all
+
+    @books = Book.new(books_params)
+    if @books.save
+      flash[:notice] = "Book was successfully created."
+    else
+      render :index
+    end
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'books/index'
   get 'books/show'
   get 'books/edit'
+  #上4つはresources :booksで一括処理できたはず
   root :to => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
