42f3010fa432a64c909fd0ec4d981a72eb570ca9
MasamichiMiyagi <114850152+MasamichiMiyagi@users.noreply.github.com>
2022-10-13 05:54:32 +0000

    [2022/10/13/14:54]bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,7 @@ class BooksController < ApplicationController
   end
 
   def create
+    @books = Book.all
     @book = Book.new(book_params)
     if @book.save
       flash[:notice] = "Book was successfully created."
@@ -24,10 +25,10 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
       flash[:notice] = "Book was successfully updated."
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
       render :edit
     end
@@ -35,10 +36,9 @@ class BooksController < ApplicationController
 
   def destroy
     book = Book.find(params[:id])
-    if book.destroy
+    book.destroy
       flash[:notice] = "Book was successfully destroyed."
-      render :index
-    end
+    redirect_to books_path
   end
 
   private #←一種の境界線、「ここから下はこのcontrollerの中でしか呼び出せません」という意味があるので、他アクション(create,index,show等)を巻き込まないように一番下に書く。
@@ -46,4 +46,5 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
+
 end
